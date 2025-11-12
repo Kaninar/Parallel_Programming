0cb30bc78077c1a0eded61b58bfd253ab815c611
Timur Makhanov <148999387+TopazZ1o@users.noreply.github.com>
2025-11-12 19:56:22 +0500

    Add files via upload

---
+++ advanced_tasks/MPI10.cpp
@@ -0,0 +1,64 @@
+#include <mpi.h>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double integral(double a, int i, double h, int n) {
+	auto fct = [](double x) {
+		return cos(x);
+		};
+	double sum = 0.0;
+	double h2 = h / 2.0;
+
+	for (int j = 0; j < n; j++) {
+		double aij = a + (i * n + j) * h;
+		sum += fct(aij + h2) * h;
+	}
+	return sum;
+}
+
+int main(int argc, char* argv[]) {
+	int n, p;
+	double h, a, b, pi;
+	int Iam, master = 0;
+	double my_result, result;
+	double start_time, end_time, elapsed_time;
+	MPI_Status status;
+	MPI_Request req[1];
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &Iam);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+	pi = acos(-1.0);
+	a = 0.0;
+	b = pi / 2.0;
+	n = 500;
+
+	int dest = master;
+	int tag = 123;
+
+	h = (b - a) / n / p;
+	my_result = integral(a, Iam, h, n);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	start_time = MPI_Wtime();
+
+	MPI_Allreduce(&my_result, &result, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+	end_time = MPI_Wtime();
+	elapsed_time = end_time - start_time;
+
+	cout << "Process " << Iam 
+		<< ", result = " << my_result
+		<< ", elapsed time = " << elapsed_time << endl;
+
+	if (Iam == master) {
+		cout << "Final result = " << std::fixed << std::setprecision(6) << result 
+			<< endl;
+	}
+
+	MPI_Finalize();
+	return 0;
+}
